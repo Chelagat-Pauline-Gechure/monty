c87850af2193077d6a0d10cdaad0b7d6f76f6280
gkiplangat <gkiplangat>
2023-06-22 23:35:56 +0300

    fix:updated push and pall

--- push.c
+++ push.c
@@ -1,21 +1,21 @@
 #include "monty.h"
 /**
- * f_push - add node to the stack
+ * full_push - add node to the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_push(stack_t **head, unsigned int counter)
+void full_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0, flag = 0;
+	int a, b = 0, flag = 0;
 
 	if (bus.arg)
 	{
 		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
+			b++;
+		for (; bus.arg[b] != '\0'; b++)
 		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+			if (bus.arg[b] > 57 || bus.arg[b] < 48)
 				flag = 1; }
 		if (flag == 1)
 		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
@@ -29,9 +29,9 @@ void f_push(stack_t **head, unsigned int counter)
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
+	a = atoi(bus.arg);
 	if (bus.lifi == 0)
-		addnode(head, n);
+		addnode(head, a);
 	else
-		addqueue(head, n);
+		addqueue(head, a);
 }
