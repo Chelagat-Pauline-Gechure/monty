2345e6749001943a7f19975930b8a83534386367
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-06-22 22:13:12 +0300

    fix:output

--- main.c
+++ main.c
@@ -1,41 +1,45 @@
 #include "monty.h"
-
-argument_t *argument = NULL;
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - Entry point
- * @argc: The number of command-line arguments.
- * @argv: A pointer to an array of strings containing
- * the command-line arguments.
- * Description: print alphabets in lowercase using the putchar
- *
- * Return: returns 0 (Success)
- */
-int main(int argc, char **argv)
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
 {
-	ssize_t lines_read = 0;
-	size_t n = 0;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-	check_num_of_arguments(argc);
-
-	argument = malloc(sizeof(argument_t));
-	if (argument == NULL)
-		malloc_failed();
-
-	init_arg();
-	setStream(argv[1]);
-
-	while ((lines_read = getline(&argument->line, &n, argument->stream)) != -1)
+	if (argc != 2)
 	{
-		argument->line_number += 1;
-		tokenize();
-		setInstruction();
-		runInstruction();
-		free_toks();
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
-
-	closeStream();
-	free_arg();
-
-	return (0);
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
--- monty.h
+++ monty.h
@@ -1,15 +1,12 @@
 #ifndef MONTY_H
 #define MONTY_H
-
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <fcntl.h>
+#include <sys/types.h>
 #include <unistd.h>
-
-#define NO_OF_INSTRUCTIONS 7
-
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -17,90 +14,65 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
-
+/**
+ * struct bus_s - variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+} bus_t;
+extern bus_t bus;
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-/**
- * struct argument_s - struct to help pass arguments around
- *
- */
-typedef struct argument_s
-{
-	char **line_strs; /* used to store tokens from line */
-	char *line; /* used for getting line from file */
-	unsigned int line_number; /* for tracking current line number */
-	instruction_t *instruction; /* a valid instruction from a line */
-	int n_tokens; /* number of tokens created from line */
-	int isComment; /* tracks whether or not tokens start with # */
-	FILE *stream; /* file stream */
-	int stack_length; /* tracks the number of nodes in the stack */
-	stack_t *stackHead; /* head/top of the stack (doubly linked lists of struct stack_s) */
-	int stack; /* for switching between the use of stack and queue */
-} argument_t;
-
-FILE *fdopen(int fd, const char *mode);
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-
-extern argument_t *argument;
-
-void push(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
-void _div(stack_t **stack, unsigned int line_number);
-void mul(stack_t **stack, unsigned int line_number);
-void mod(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
-void rotl(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, unsigned int line_number);
-void stack(stack_t **stack, unsigned int line_number);
-void queue(stack_t **stack, unsigned int line_number);
-
-void check_num_of_arguments(int argc);
-void read_failed(char *fileName);
-void malloc_failed(void);
-void free_all_args(void);
-void free_arg(void);
-void free_toks(void);
+char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t getstdin(char **lineptr, int file);
+char *clean_line(char *content);
+void f_push(stack_t **head, unsigned int number);
+void f_pall(stack_t **head, unsigned int number);
+void f_pint(stack_t **head, unsigned int number);
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
-void delete_stack_node(void);
-void closeStream(void);
-void setStream(char *fileName);
-void setInstruction(void);
-void invalid_instruction(void);
-void runInstruction(void);
-void free_stackHead(void);
-void tokenize(void);
-void init_arg(void);
-
-
-int is_comment();
-int is_number(char *str);
-
-
-#endif /* MONTY_H */
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+void f_sub(stack_t **head, unsigned int counter);
+void f_div(stack_t **head, unsigned int counter);
+void f_mul(stack_t **head, unsigned int counter);
+void f_mod(stack_t **head, unsigned int counter);
+void f_pchar(stack_t **head, unsigned int counter);
+void f_pstr(stack_t **head, unsigned int counter);
+void f_rotl(stack_t **head, unsigned int counter);
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
+void f_queue(stack_t **head, unsigned int counter);
+void f_stack(stack_t **head, unsigned int counter);
+#endif
